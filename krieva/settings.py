bb7a3ea6ee30fd23bba9e25474733b35b75d8a70
Tanmay Ambadkar <tanmay.ambadkar@gmail.com>
2020-10-30 13:21:03 +0530

    added .gitignore and decouple

--- krieva/settings.py
+++ krieva/settings.py
@@ -11,16 +11,15 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#ije1#1q99bs+0$qrjo@4qg7rxyyj_wkx&yaa_s8r#slgd_!v1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
