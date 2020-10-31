82f03cbd4c2f22e5be46e1e1da61287d04b433e7
Tanmay Ambadkar <tanmay.ambadkar@gmail.com>
2020-10-31 12:41:31 +0530

    x

--- krieva/settings.py
+++ krieva/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'krieva.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates', ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
