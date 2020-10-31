0a6ddebbd3c6bbdb9378a7e85ba1a1d7d7c935ef
Tanmay Ambadkar <tanmay.ambadkar@gmail.com>
2020-10-31 12:46:23 +0530

    x

--- krieva/settings.py
+++ krieva/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('SECRET_KEY')
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['kreiva.herokuapp.com', 'localhost']
 
