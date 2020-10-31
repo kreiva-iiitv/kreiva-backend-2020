9de63c031bd0e7230c615d6576ac79899363a499
Tanmay Ambadkar <tanmay.ambadkar@gmail.com>
2020-10-31 11:58:41 +0530

    'update'

--- krieva/settings.py
+++ krieva/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import django_heroku
+
+django_heroku.settings(locals())
+
 
 SECRET_KEY = config('SECRET_KEY')
 
