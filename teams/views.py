16b07a23cd7fd40922368bd20f8d262c133c7a19
Krishna Gaur <gaurkrishna498@gmail.com>
2021-01-14 21:51:22 +0530

    Team list api completed

--- teams/views.py
+++ teams/views.py
@@ -5,13 +5,29 @@ from rest_framework.response import Response
 from teams.models import *
 from teams.serializers import *
 from teams.forms import *
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 
 
 
 class TeamViewSet(viewsets.ModelViewSet):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
+
+    def list(self, request):
+        teams = []
+        for team in self.queryset:
+            teamData = self.get_serializer(team)
+            resppnseData = {}
+            resppnseData.update(teamData.data)
+
+            member = TeamMember.objects.filter(team=team).order_by('-rolepriority')
+            if len(member) == 0:
+                member = TeamMember(team=team)
+            memberData = TeamMemberSerializer(member, many=True)
+            resppnseData['teamMembers'] = memberData.data
+            teams.append(resppnseData)
+        return Response({'Team': teams}, status=status.HTTP_200_OK)            
+
 
 
 class TeamMemberViewSet(viewsets.ModelViewSet):
